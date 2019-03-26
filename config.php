8ebe9b55442fa3e4f5ce6cabed7c6463f95a971d
Randy Rivera <randyr7k@gmail.com>
2019-03-25 21:36:59 -0400

    initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+   define('DB_SERVER', 'jackpot-conectado.database.windows.net');
+   define('DB_USERNAME', 'jackpot');
+   define('DB_PASSWORD', 'Zr0bj216');
+   define('DB_DATABASE', 'SQL SERVER');
+   $db = mysqli_connect(jackpot-conectado.database.windows.net,jackpot,Zr0bj216,SQL SERVER);
+
+   function create_uesr($user){
+     // 
+   }
+?>
